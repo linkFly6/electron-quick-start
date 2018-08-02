e4cddbfca2673464d21bd799731c54a3077ead2f
linkFly <linkFly6@live.com>
2018-08-02 20:26:08 +0800

    build 环境完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,47 @@
-var gulp = require('gulp')
-var watch = require('gulp-watch')
+/**
+ * 感谢文章
+ * https://zhanglun.github.io/2016/08/05/Electron%20%E5%BC%80%E5%8F%91%E7%9A%84%20gulp%20%E9%85%8D%E7%BD%AE/
+ * 提供的思路
+ */
+const gulp = require('gulp')
+const electron = require('electron-connect').server.create()
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js')
+const PluginError = require('plugin-error');
+const log = require('fancy-log');
 
-gulp.task('webpack', function () {
-    
+const devCompiler = webpack(webpackConfig)
+
+gulp.task('webpack:build', function () {
+  // Reload renderer process
+  devCompiler.run(function (err, status) {
+    if (err) {
+      throw PluginError('webpack:build-dev', err, { showStack: true });
+    }
+    /**
+     * 这里第一次导致重新 reload 一遍
+     */
+    electron.reload()
+    log('[webpack:build-dev]', status.toString({
+      colors: true
+    }));
+  });
 })
+
+
+gulp.task('watch', function () {
+
+  // Start browser process
+  electron.start()
+
+  // Restart browser process
+  gulp.watch('src/main/**', electron.restart)
+
+  gulp.watch('src/renderer/**', ['webpack:build'])
+})
+
+
+/**
+ * 默认任务，先 watch 让 electron start，然后 build
+ */
+gulp.task('default', ['watch', 'webpack:build'])
