c91b43d75b8cf26e6a4860441ac694cb395d9da6
linkFly <linkFly6@live.com>
2018-08-02 22:26:31 +0800

    开始

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,18 +1,17 @@
+import '@/assets/css/app.styl';
 import * as React from 'react';
 import {
   BrowserRouter,
   Route,
   Switch,
-  Redirect,
-  withRouter,
-  RouteComponentProps,
 } from 'react-router-dom';
 import { Router } from 'react-router';
-import { store } from './store';
 import history from './assets/js/history';
 import { observer } from 'mobx-react';
 import HomeView from './views/home';
 import DemoView from './views/demo';
+import Directory from './views/partial/directory';
+
 
 @observer
 export default class App extends React.Component<any, any> {
@@ -21,12 +20,17 @@ export default class App extends React.Component<any, any> {
       <BrowserRouter>
         <Router history={history}>
           <div className="app">
-            <Switch>
-              <Route path="/demo" component={DemoView} />
-              <Route component={HomeView} />
-            </Switch>
-            {/* 妈蛋 preact 没有对应的 mobox-react-devtools 工具 */}
-            {/* {DevTools && search<{ debug?: string }>().debug ? <DevTools /> : null} */}
+            <div className="app-directory">
+              <Route component={Directory} />
+            </div>
+            <div className="app-content">
+              <Switch>
+                <Route path="/demo" component={DemoView} />
+                <Route component={HomeView} />
+              </Switch>
+              {/* 妈蛋 preact 没有对应的 mobox-react-devtools 工具 */}
+              {/* {DevTools && search<{ debug?: string }>().debug ? <DevTools /> : null} */}
+            </div>
           </div>
         </Router>
       </BrowserRouter>
---
+++ src/renderer/views/partial/directory/index.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import { observer, inject } from 'mobx-react';
+import './style.styl';
+import UserStore from '@/store/user-store';
+import { BaseComponent } from '@/components/base-component';
+import { RouteProps } from 'react-router-dom';
+// import { withStyles } from '@material-ui/core/styles';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
+
+export interface IProps {
+  userStore: UserStore;
+  match: any;
+}
+
+interface IState {
+  open: boolean;
+}
+
+@inject((allStores: any) => ({
+  userStore: allStores.store.userStore,
+}))
+@observer
+export default class Home extends BaseComponent<IProps & RouteProps, IState> {
+  public state = { open: true };
+
+  public handleClick = () => {
+    this.setState(state => ({ open: !state.open }));
+  }
+  public render() {
+    return (
+      <div className={'component-directory'}>
+        <List
+          component="nav"
+          subheader={<ListSubheader component="div">Nested List Items</ListSubheader>}
+        >
+          <ListItem button>
+            <ListItemIcon>
+              <SendIcon />
+            </ListItemIcon>
+            <ListItemText inset primary="Sent mail" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText inset primary="Drafts" />
+          </ListItem>
+          <ListItem button onClick={this.handleClick}>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText inset primary="Inbox" />
+            {this.state.open ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItem button>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText inset primary="Starred" />
+              </ListItem>
+            </List>
+          </Collapse>
+        </List>
+      </div>
+    );
+  }
+}
